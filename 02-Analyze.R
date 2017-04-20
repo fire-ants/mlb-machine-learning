059520d4cb6dc7bd6b9124eba2390fbe8df78573
jrbattles <jason.r.battles@gmail.com>
2017-04-20 10:32:11 -0500

    inital analysis. defined target machine learning variables.

--- 02-Analyze.R
+++ 02-Analyze.R
@@ -1,16 +1,15 @@
 ## Conduct analysis
 
 # Mike Trout
-batsTrout <- filter(atbat16, batter == "518626")
+batsTrout <- filter(atbat16, batter == "545361")
 #FBs <- filter(pitch11, pitch_type == "FF" | pitch_type == "FC")
 
 # inner join all pitches to specific batter
 pitchesTrout <- collect(inner_join(pitch16, batsTrout))
 
-# add new columns for HitterVal
-#joined <- pitchesTrout %>% mutate(quant_score = get_quant_score(des),
-#                                  qual_score = get_qual_score(atbat_des) * (type == 'X'),
-#                                  hitter_val = quant_score + qual_score)
+# 3028 pitches thrown to Trout
+nrow(pitchesTrout)
+
 
 joined <- pitchesTrout %>% mutate(quant_score_des = get_quant_score(des),
                                   fix_quant_score = fix_quant_score(event) * (des == 'In play, run(s)'),
@@ -24,3 +23,10 @@ joined$des <- as.factor(joined$des)
 joined$type <- as.factor(joined$type)
 joined$count <- as.factor(joined$count) 
 joined$event <- as.factor(joined$event) 
+
+###  Commenceth thou Machine Learning.   Predict how to minimize HitterVal, or alternatively, how to minimize type = X
+#  Interesting variables from pitch
+# tfs, tfs_zulu, id, px, pz, pitch_type, count, zone, nasty
+
+# Interesting variables from atbat
+# p_throws, inning_side
