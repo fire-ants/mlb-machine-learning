8085dcdb9b39e4254717f65eccb11441df28ea7c
Goyette <Colin.Goyette@emc.com>
2018-02-07 17:06:57 -0500

    cleaned and formatted .R script, .ipynb, .py files for legibility

--- 00-DataAcquistion.R
+++ 00-DataAcquistion.R
@@ -10,18 +10,7 @@ library(lubridate)
 setwd("/db")
 my_dbProd <- src_sqlite("pitchRxProd.sqlite3", create = FALSE)
 
-Today <- Sys.Date()
-NinetyDaysAgo <- Today - 90
-
-## scrape game data and store in the database
-#scrape(start = NinetyDaysAgo, end = Today, suffix = "inning/inning_all.xml", connect = my_dbProd$con)
-
-# To speed up execution time, create an index on these three fields.
-#dbSendQuery(my_dbProd$con, "CREATE INDEX url_atbat ON atbat(url)") 
-#dbSendQuery(my_dbProd$con, "CREATE INDEX url_pitch ON pitch(url)")
-#dbSendQuery(my_dbProd$con, "CREATE INDEX pitcher_index ON atbat(pitcher_name)")
-#dbSendQuery(my_dbProd$con, "CREATE INDEX des_index ON pitch(des)")
-
+#Create R dataframes from my_dbProd
 pitch16 <- select(tbl(my_dbProd, "pitch"), gameday_link, num, des, type, tfs, tfs_zulu, id, end_speed, pitch_type, count, zone)
 atbat16 <- select(tbl(my_dbProd, "atbat"), gameday_link, num, pitcher, batter, b_height, pitcher_name, p_throws, batter_name, stand, atbat_des, event, inning, inning_side)
 
@@ -103,41 +92,14 @@ levels(joined$pitch_type)[levels(joined$pitch_type)=="KC"] <- "KN"
 # Decide Good (1) or Bad (0) -- if the hitterval score is negative, we award this as "1" aka, good for pitcher
 joined.classic <- joined %>% mutate(hv_binary = ifelse(hitter_val < 0, 1, 0))
 
-
 #categorical data prep - create zone and pitch type pairs for one-hot encoding -- credit to Benita Mordi for this idea!!
 joined.classic <- joined.classic %>% mutate(ptz=paste(pitch_type,zone, sep = "_"))
 
 #reduce feature space - remove infrequent pitch types
 joined.classic.pitchedit <- joined.classic %>% filter(pitch_type != c('EP','FO','PO','SC'))
 
-#view missing data -- visualize missing data, part of exploratory search for best feature space
-#visna(joined.classic, tp = TRUE, col = "blue")
-
-#create subsets of pitcher stance and batter stance 
-#Rh <- joined.classic %>% filter(stand == "R")
-#Lh <- joined.classic %>% filter(stand == "L")
-
-#Rpitch <- joined.classic %>% filter(p_throws == "R")
-#Lpitch <- joined.classic %>% filter(p_throws =="L")
-
-#RhRp <- Rh %>% filter(p_throws == "R")
-#RhLp <- Rh %>% filter(p_throws == "L")
-#LhRp <- Lh %>% filter(p_throws == "R")
-#LhLp <- Lh %>% filter(p_throws == "L")
-
-#More exploratory visualizations: Primary Component Plots... need to update table names per above convention
-#PCP <- ggparcoord(data = joined_classic[order(joined_classic$hv_binary, decreasing = FALSE),], columns = c(40,46,30,16,17,32,88), groupColumn = "hv_binary", title = "Factors v Pitcher Outcome", alpha = .01) PCP_cat <- ggparcoord(data = joined.temp[order(joined.temp$GoodBadQual, decreasing = TRUE),], columns = c(40,46,30,32,88), groupColumn = "GoodBadQual", title = "Categorical Factors v Pitcher Outcome")
-#RpRh_pcp <- ggparcoord(data = RhRp[order(RhRp$hv_binary, decreasing = FALSE),], columns = c(8,9,11,14,27,28), groupColumn = "hv_binary", title = "RpRh PCP v Pitcher Outcome")
-#RpLh_pcp <- ggparcoord(data = RpitchLh[order(RpitchLh$GoodBadQual, decreasing = TRUE),], columns = c(16,17,30,32,40,46,88), groupColumn = "GoodBadQual", title = "RpLh PCP v Pitcher Outcome")
-#LpRh_pcp <- ggparcoord(data = LpitchRh[order(LpitchRh$GoodBadQual, decreasing = TRUE),], columns = c(16,17,30,32,40,46,88), groupColumn = "GoodBadQual", title = "LpRh PCP v Pitcher Outcome")
-#LpLh_pcp <- ggparcoord(data = LpitchLh[order(LpitchLh$GoodBadQual, decreasing = TRUE),], columns = c(16,17,30,32,40,46,88), groupColumn = "GoodBadQual", title = "LpLh PCP v Pitcher Outcome")
-
-#create data for Swinging Strikes outside the strike zone. 
-#SS_NonSZ_Rh <- Rpitch   %>% filter (des == "Swinging Strike" & zone == c(11,12,13,14))
-#SS_NonSZ_Lh <- Lpitch %>% filter (des == "Swinging Strike" & zone == c(11,12,13,14))
-
 #export features of interest, with hv_binary label 1 if <0, else 0
-var.interest <- joined.classic.pitchedit %>% select(3,5,6,8:13,16,18,22,27:29)
+  var.interest <- joined.classic.pitchedit %>% select(12,13,28,29)
 #write.csv(var.interest, file = paste(format(Sys.Date(), "HVal-%Y-%m-%d"), "csv", sep = "."))
 write.csv(var.interest, file = "rawdata_ML.csv")
 
--- 00-MachineLearn.py
+++ 00-MachineLearn.py
@@ -1,27 +1,32 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[3]:
 
 
-from sklearn import datasets
+#SKlearn for logistic regression modeling
+
+#from sklearn import datasets
 from sklearn import preprocessing
 from sklearn.datasets import make_classification
 from sklearn.ensemble import (ExtraTreesClassifier, RandomTreesEmbedding, RandomForestClassifier)
 from sklearn.preprocessing import OneHotEncoder
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas
 
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
 from sklearn import linear_model, decomposition, datasets
 
+#numpy, pandas for data manipulation
+import numpy as np
+import pandas as pd
+
+#import sympy for formulaic expression of log-odds ratios, e.g., to produce % success rates from coefficients of logit reg
 import sympy
 from sympy.solvers import solve
 from sympy import Symbol
 
-import requests # importing the requests library
+# importing the requests library
+import requests 
 import json
 import os
 
@@ -32,10 +37,10 @@ import os
 os.chdir("/db")
 
 
-# In[2]:
+# In[4]:
 
 
-#os.getcwd()
+os.getcwd()
 
 
 # In[ ]:
@@ -44,22 +49,17 @@ os.chdir("/db")
 #[514888,453568,457759,519317,458015,547180,641355,592450,545361,457705,502671,518626,502517,518934,471865,592178,519346]
 
 
-# In[20]:
+# In[5]:
 
 
 def hv_model(features):
     
-    #"rawdata_ML.csv"
-    
-    #import data, remove rows with NA values
-    raw = pandas.read_csv("rawdata_ML.csv", encoding = "utf-8-sig").dropna(axis=0)
-
-    #identify unique batter ids in dataset
-    Batters = raw.batter.unique()
+    #import data, remove rows with NA values 
+    raw = pd.read_csv("rawdata_ML.csv", encoding = "utf-8-sig").dropna(axis=0)
     
     #Note: opportunity to pass in argument of batters in the future
     Batters_list = [514888,453568,457759,519317,458015,547180,641355,592450,545361,457705,502671,518626,502517,518934,471865,592178,519346]
-    
+
     #identify pitcher handedness. Like Jason has yet to see my ambidextrosity, we have yet to see anything more than "L" or "R", but we prefer this method to hard coding :P
     P_throws = raw.p_throws.unique()
     
@@ -77,14 +77,8 @@ def hv_model(features):
     
             #select features to incorporate into model based on input argument
             var_interest = records[features]
-
-            #zone is categorical, prep for encoding
-            if 'zone' in var_interest:
-                var_interest.loc[:,'zone'] = var_interest[:,'zone'].astype(object)   
-            else:
-                pass
             
-             #ptz variable (pitch_zone combo) is categorical, prep for encoding
+            #ptz variable (pitch_zone combo) is categorical, prep for encoding
             if 'ptz' in var_interest:
                 var_interest.ptz = var_interest.ptz.astype(object)   
             else:
@@ -115,66 +109,23 @@ def hv_model(features):
 
 
             #Create one_hot encoded predictor array for logistic regression
-            X_hot = pandas.get_dummies(X)
+            X_hot = pd.get_dummies(X)
             
-            #note that we do not need to create train/test splits for this case, as we are interested in the coefficientso of the model, not predicting non-classified results
-            #the code to do so follows anyway
-            
+            #note that we do not need to create train/test splits for this case, as we are interested in the coefficients of the model, not predicting non-classified results            
             #Split up train and test data, 80/20 split
             #X_hot_train, X_hot_test, y_train, y_test = train_test_split(X_hot, Y, test_size = 0.3)
 
-            #initiate ExtraTreesClassifier for feature importance estimation. 
-
-            #From sklearn doc: This class implements a meta estimator that fits a number of randomized decision trees (a.k.a. extra-trees) 
-            #on various sub-samples of the dataset and use averaging to improve the predictive accuracy and control over-fitting.
-            
-            #forest = ExtraTreesClassifier(n_estimators = len(X_hot.columns), random_state = None)
-            
-            #fit ExtraTreesClassifier to encoded categorical indicators, pitch type to binary hitter_val Y
-            #forest.fit(X_hot, Y.values.ravel())
-            
-            #Net feature importance- NOTE that this does not discern between good or bad outcome, just importance to model
-            #code borrowed and modified for visualization of ExtraTrees classifier, from sci-kit learn website: http://scikit-learn.org/stable/auto_examples/ensemble/plot_forest_importances.html#sphx-glr-auto-examples-ensemble-plot-forest-importances-py
-            
-            #importances = forest.feature_importances_
-            #std = np.std([tree.feature_importances_ for tree in forest.estimators_], axis=0)
-            #indices = np.argsort(importances)[::-1]
-
-            # Print the feature importance ranking, from ExtraTreesClassifier
-            #print("Feature ranking:")
-
-            #for f in range(X_hot_test.shape[1]):
-            #    print("%d. feature %d (%f)" % (f + 1, indices[f], importances[indices[f]]))
-
-            # Plot the feature importances of the forest
-            #plt.figure(figsize=(10,5))
-            #plt.title("Feature importances")
-            #plt.bar(range(X_hot_test.shape[1]), importances[indices], color="g", yerr=std[indices], align="center")
-            #plt.xticks(range(X_hot_test.shape[1]), indices)
-            #plt.xlim([-1, X_hot_test.shape[1]])
-            #plt.show()
-            
-            #print("Decode features from ExtraTreesClassifier:")
-            #for c in X_hot.columns:
-             #   print (X_hot.columns.get_loc(c), c)
-
-                
-                
-            #Logistic regression model, initialize
+            #Logistic regression model, initialize function from sklearn
             logit_reg = linear_model.LogisticRegression()
 
             #linear regression on one-hot encoded data X and Y HVAL labels
             model = logit_reg.fit(X_hot, Y.values.ravel())
 
-
-            #Average success, from pitcher's perspective
+            #Average success, from pitcher's perspective.  Note that this is just the baseline likelihood of predicting the correct outcome by chance. We compare our model accuracy to this value.
             avg_success=Y.mean().values[0]
-            #print avg_success
-            #print Y.mean()[0], Y.mean()[1]
         
             #Baseline pitcher success rate
-        
-            #print "Random %s-handed pitcher's baseline success ratio against hitter:" % (hand, , Y.mean()
+            #print("Random %s-handed pitcher's baseline success ratio against hitter:" % (hand, , Y.mean())
             if hand == 'R':
                     RHPfindingslist.append(("Based on the last 90 days' worth of pitches against this batter, %s-handed pitchers have a %s success rate." % (hand,"{0:.0f}%".format(avg_success* 100))))
             elif hand == 'L':
@@ -182,43 +133,36 @@ def hv_model(features):
             else:
                 pass
          
-        
-        
             #logistic regression results
-            Results = pandas.DataFrame(list(zip(X_hot.columns, np.transpose(model.coef_), np.transpose(np.exp(model.coef_)), abs(np.transpose(np.exp(model.coef_)-1)))))
+            Results = pd.DataFrame(list(zip(X_hot.columns, np.transpose(model.coef_), np.transpose(np.exp(model.coef_)), abs(np.transpose(np.exp(model.coef_)-1)))))
             
-            Results.columns = ['Suggestion', 'LR_coeff/Log_Odds', 'Odds_Ratio', 'abs_pi']
+            Results.columns = ['Recommendation', 'LR_coeff/Log_Odds', 'Odds_Ratio', 'Abs_Odds_Ratio_-1']
     
             #sorted results
-            Results = Results.sort_values(by='abs_pi', ascending = False)
-            Top_5 = Results[['Suggestion','Odds_Ratio']][:5]
+            Results = Results.sort_values(by='Abs_Odds_Ratio_-1', ascending = False)
+            Top_5 = Results[['Recommendation','Odds_Ratio']][:5]
             Top_5.Odds_Ratio = Top_5.Odds_Ratio.astype(float)
             
             
-            
-            #below loop is to translate logistic regression coefficients into odds ratio, e.g., improved odds of success of choosing this feature
+            #Use sympy to translate logistic regression coefficients into odds ratio, e.g., improved odds of success of choosing this feature
             x = Symbol('x')
             
             for index,row in Top_5.iterrows():
-              
+                  
+                #log-odds ratio calc
                 Top_5.loc[index,'New_Odds'] = solve(Top_5.loc[index,'Odds_Ratio']-((x/(1-x))/(Y.mean()/(1-Y.mean()))), x)
             
             #string formatting
             Top_5['New_Odds']= Top_5['New_Odds'].str.get(0)
-            #print Top_5['New_Odds'], Top_5['Odds_Ratio']
+            #print(Top_5['New_Odds'], Top_5['Odds_Ratio'])
                 
-            
-            
             #Creating new series pt:pitch_type and zc:zone_catcher, e.g., zone from catcher's perspective
             #Credit to Benita!
-            s= Top_5['Suggestion'].apply(lambda x: x.split('_'))
+            s= Top_5['Recommendation'].apply(lambda x: x.split('_'))
             Top_5['pt'] = s.apply(lambda x: x[1])
             Top_5['zc'] = s.apply(lambda x: x[2])
             
-            
-            
-            #Translation of codes to descriptive terms, pitch type and zone
-            
+            #Translation of codes to descriptive terms: pitch type
             def applyFunc(s):
                 if s == 'FF':
                     return 'four-seam fastball'
@@ -236,8 +180,8 @@ def hv_model(features):
                     return 'two-seam fastball'
                 return ''
 
-            #create series of descriptive pitch type labels
-            Top_5['pd'] = Top_5['pt'].apply(applyFunc)
+            #create series of descriptive pitch type: zones
+            Top_5['pitch_descrip'] = Top_5['pt'].apply(applyFunc)
 
             def applyFunc2(s):
                 if s == '1':
@@ -271,50 +215,38 @@ def hv_model(features):
                 return ''
 
             #create series to describe zones, mirroring to be seen from pitcher's perspective
-            Top_5['zd'] = Top_5['zc'].apply(applyFunc2)    
+            Top_5['zone_descrip'] = Top_5['zc'].apply(applyFunc2)    
                  
             
             
             #Print the results!
             
-            print("Batter ID: %s" % (batter_id))
-            print("")
-            print("Based on the last 90 days' worth of pitches against this batter, %s-handed pitchers have a %s success rate." % (hand,"{0:.0f}%".format(avg_success* 100)))
-            print ("")
+            #print("Batter ID: %s" % (batter_id))
+            #print("")
+            #print("Based on the last 90 days' worth of pitches against this batter, %s-handed pitchers have a %s success rate." % (hand,"{0:.0f}%".format(avg_success* 100)))
+            #print("")
             
-            for index,row in Top_5.iterrows():
-                print("Throw a %s %s for a success rate of %s." % (Top_5.loc[index,'pd'], Top_5.loc[index, 'zd'], "{0:.0f}%".format(Top_5.loc[index,'New_Odds'] * 100)))
+            #for index,row in Top_5.iterrows():
+             #   print("Throw a %s %s for a success rate of %s." % (Top_5.loc[index,'pitch_descrip'], Top_5.loc[index, 'zone_descrip'], "{0:.0f}%".format(Top_5.loc[index,'New_Odds'] * 100)))
                 
-                if hand == 'R':
-                    RHPfindingslist.append("Throw a %s %s for a success rate of %s." % (Top_5.loc[index,'pd'], Top_5.loc[index, 'zd'], "{0:.0f}%".format(Top_5.loc[index,'New_Odds'] * 100)))
+              #  if hand == 'R':
+               #     RHPfindingslist.append("Throw a %s %s for a success rate of %s." % (Top_5.loc[index,'pitch_descrip'], Top_5.loc[index, 'zone_descrip'], "{0:.0f}%".format(Top_5.loc[index,'New_Odds'] * 100)))
                     
-                elif hand == 'L':
-                    LHPfindingslist.append("Throw a %s %s for a success rate of %s." % (Top_5.loc[index,'pd'], Top_5.loc[index, 'zd'], "{0:.0f}%".format(Top_5.loc[index,'New_Odds'] * 100)))
-                else:
-                    pass
+               # elif hand == 'L':
+                #    LHPfindingslist.append("Throw a %s %s for a success rate of %s." % (Top_5.loc[index,'pitch_descrip'], Top_5.loc[index, 'zone_descrip'], "{0:.0f}%".format(Top_5.loc[index,'New_Odds'] * 100)))
+                #else:
+                #    pass
             
-            print("")
-            print("Note: Model Accuracy, based on %s pitches:" % num_events, model.score(X_hot, Y))
-            print ("")
-            print ("")
-           
-            #print model.decision_function(X_hot)
-        
-            #payload = {'findings': ['Throw a curve ball to the top right', 'Throw a curve ball down the middle']}
-            #findingsList = ('Throw a curve ball to the top right', 'Throw a curve ball down the middle')
-        
-   
-        
-        
+            #print("")
+            #print("Note: Model Accuracy, based on %s pitches:" % num_events, model.score(X_hot, Y))
+            #print("")
+            #print("")
+            
         #load data to object store
         
-             #Note- this is dictionary containing findings results per pitcher. 
+        #Note- this is dictionary containing findings results per pitcher. 
         findingsDict = {'leftyFindings': LHPfindingslist, 'rightyFindings': RHPfindingslist}
-        #print(findingsDict.items())
-        
-        #payload = json.dumps(findingsDict)
-        #print(payload)
-        
+
         # api-endpoint
         URL = 'http://mlb-player-api.cfapps.io/player/%d/insight' % (batter_id)
         try:
@@ -324,11 +256,17 @@ def hv_model(features):
         except requests.exceptions.HTTPError as err:
             print(err)
 
-        print ("")
+        print("")
 
 
-# In[21]:
+# In[8]:
 
 
 hv_model(['ptz','hv_binary'])
+
+
+# In[1]:
+
+
+findingsDict.items()
 
